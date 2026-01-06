ac033b9e6aa642ca532cfd04f051ddd6e18d9954
Pedro Marques <120110565+ryopedrodev@users.noreply.github.com>
2026-01-06 10:23:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-// As frases que vão alternar
 const frases = ["profissionais da saúde", "academias", "comércios", "construção civil", "prestadores de serviços"];
 
-let i = 0; // Índice da frase atual
-let j = 0; // Índice da letra atual
-let isDeleting = false; // Se está apagando ou escrevendo
+let i = 0; 
+let j = 0; 
+let isDeleting = false; 
 let elemento = document.getElementById("texto-digitado");
 
 function digitar() {
     const fraseAtual = frases[i];
 
     if (isDeleting) {
-        // APAGANDO
+        
         elemento.textContent = fraseAtual.substring(0, j - 1);
         j--;
-        // Se acabou de apagar tudo, passa para a próxima frase e começa a escrever
+        
         if (j === 0) {
             isDeleting = false;
-            i = (i + 1) % frases.length; // Passa para a próxima (ou volta para a primeira)
+            i = (i + 1) % frases.length; 
         }
     } else {
-        // ESCREVENDO
+        
         elemento.textContent = fraseAtual.substring(0, j + 1);
         j++;
-        // Se acabou de escrever a frase inteira, espera um pouco e começa a apagar
+        
         if (j === fraseAtual.length) {
             isDeleting = true;
-            setTimeout(digitar, 2000); // Pausa de 2 segundos antes de apagar (LEIA AQUI)
-            return; // Sai da função para respeitar o tempo
+            setTimeout(digitar, 2000); 
+            return; 
         }
     }
 
-    // Velocidade: Se estiver apagando é mais rápido (50ms), escrevendo é normal (100ms)
+    
     const velocidade = isDeleting ? 50 : 100;
     setTimeout(digitar, velocidade);
 }
 
-// Inicia o efeito
+
 digitar();
 
 function verificarHorario() {
     const data = new Date();
     const hora = data.getHours();
-    const dia = data.getDay(); // 0 = Domingo, 6 = Sábado
+    const dia = data.getDay(); 
 
     const elementoStatus = document.getElementById("status-loja");
     const textoStatus = document.getElementById("texto-status");
 
-    // Lógica: Aberto de Seg (1) a Sex (5), das 09h às 17h
     const ehDiaUtil = dia >= 1 && dia <= 5;
     const ehHorarioComercial = hora >= 9 && hora < 17;
 
@@ -61,13 +59,11 @@ function verificarHorario() {
     }
 }
 
-// Roda a função assim que carrega
 verificarHorario();
 
-// Pega o ano atual do sistema automaticamente
+
 document.getElementById("ano-atual").innerText = new Date().getFullYear();
 
-/* ATUALIZA O ANO DO RODAPÉ AUTOMATICAMENTE */
 const anoElement = document.getElementById("ano-atual");
 if (anoElement) {
     anoElement.innerText = new Date().getFullYear();
